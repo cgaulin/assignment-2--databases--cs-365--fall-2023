8cc83f9ae93ff8f605bc3c67840d0c2d3361be5d
cgaulin <cgaulin@hartford.edu>
2023-10-27 13:51:21 -0400

    changed to a single password manager table

--- setup.sql
+++ setup.sql
@@ -4,24 +4,28 @@ DROP DATABASE IF EXISTS passwords;
 
 CREATE DATABASE passwords;
 
+SET time_zone = '+00:00';
+
+SET block_encryption_mode = 'aes-256-cbc';
+SET @key_str = UNHEX(SHA2('password123', 512));
+SET @init_vector = RANDOM_BYTES(16);
+
 USE passwords;
 
-CREATE TABLE website (
-    website_URL VARCHAR(128) NOT NULL,
+CREATE TABLE pass_word_manager (
+    id_number AUTO_INCREMENT,
     website_name VARCHAR(128) NOT NULL,
-    PRIMARY KEY (website_URL) 
-);
-
-CREATE TABLE user (
+    website_URL VARCHAR(128) NOT NULL,
     first_name VARCHAR(64) NOT NULL,
     last_name VARCHAR(64) NOT NULL,
     username VARCHAR(64) NOT NULL,
     email VARCHAR(128) NOT NULL,
-    PRIMARY KEY (username, website_URL)
+    p_word VARBINARY(128) NOT NULL,
+    comment TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id_number) 
 );
 
-CREATE TABLE password (
-    p_word VARCHAR(128) NOT NULL,
-    t_stamp DECIMAL(5, 2) NOT NULL,
-    PRIMARY KEY (p_word, username, website_URL)
-);
+INSERT INTO pass_word_manager (website_name, website_URL, first_name, last_name, username, email p_word, comment) VALUES
+('Youtube', 'https://www.youtube.com', 'Cameron', 'Gaulin', 'cgaulin', 'cam@gmail.com', AES_ENCRYPT('youtubepassword', @key_str, @init_vector),
+'This is definitely not my youtube password');
